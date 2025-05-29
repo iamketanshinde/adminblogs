f8e45830f4778e68612e207ec9f2c853f3b4d046
Ketan Shinde <ketan_shinde@hotmail.com>
2025-05-29 17:43:46 +0530

    signup and signin route is created

--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,23 @@
 const Router = require('express');
 
 const router = Router();
+const Admin = require("../models/admin")
+
+router.get("/signin",(req,res)=>{
+    return res.render(signin);
+})
+router.get("/signup",(req,res)=>{
+    return res.render(signup);
+})
+
+router.post("/signup", async(req,res)=>{
+    const {fullname, email, password} = req.body;
+    await Admin.create({
+        fullname,
+        email,
+        password,
+    });
+    return res.redirect("/")
+})
 
 module.exports = router;
