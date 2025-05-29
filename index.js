6cfb36f4108719ba8ffc7d46da63fd4f421c1b44
Ketan Shinde <ketan_shinde@hotmail.com>
2025-05-29 21:29:13 +0530

    mongoose collection is fixed and signup form is also ready for post!

--- index.js
+++ index.js
@@ -4,16 +4,21 @@ const adminroute = require("./routes/admin")
 
 const app = express();
 const PORT = 3000;
+const mongoose = require("mongoose");
 
 app.set("view engine", "ejs");
 app.set("views",path.resolve('./views'));
-
+mongoose.connect("mongodb://127.0.0.1:27017/adminblogs")
+.then(e => console.log("MongoDb Connected On 27017 !!."))
+.catch(Error=> console.log(`${Error} in MongoDb Connection?`));
 
 app.get("/",(req,res) => {
     res.render("dashboard")
 })
 
-app.use("/admin",adminroute)
+app.use("/admin",(req,res)=>{
+    res.render("signup")
+},adminroute)
 
 
 app.listen(PORT,()=>console.log(`Server Running On :- ${PORT} !!.`));
