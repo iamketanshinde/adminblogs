168e78b19f4e7a6a74482adf07dfddd47f42c86b
Ketan Shinde <ketan_shinde@hotmail.com>
2025-06-22 23:44:01 +0530

    added blog model

--- models/blog.js
+++ models/blog.js
@@ -1,1 +1,23 @@
-const expr
+const {Schema, model} = require("mongoose");
+
+const blogSchema = new Schema({
+    title:{
+        type:"String",
+        trim:true,
+        require:true,
+    },
+    content:{
+        type:"String",
+        require:true,
+    },
+    createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    },
+    createdAt: {
+    type: Date,
+    default: Date.now,
+  }
+},{timestamps:true});
+
+module.exports = mongoose.model("blog",blogSchema);
